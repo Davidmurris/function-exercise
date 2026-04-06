7c704ca05b07b64ec2ef190d8a3560f61399b956
Davidmurris <141466955+Davidmurris@users.noreply.github.com>
2026-04-06 13:23:24 +0100

    Update question17.js

---
+++ question17.js
@@ -0,0 +1,17 @@
+function calculateBMI(number){
+    if (number < 18.5){
+        return 'underweight'
+    }
+    else {
+        return 'overweight'
+    }
+}
+
+if (number > 18.5 >= 24.9) {
+    return 'normalWeight'
+    
+} else {
+    return 'overweight'
+}
+
+calculateBMI(39)
